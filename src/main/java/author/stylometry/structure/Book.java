c017431d34c38737e9ee5033692d9bebd103d934
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-07 21:55:48 -0700

    Add Book.java

--- src/main/java/author/stylometry/structure/Book.java
+++ src/main/java/author/stylometry/structure/Book.java
@@ -1,5 +1,44 @@
 package main.java.author.stylometry.structure;
 
-public class Book {
+import java.util.ArrayList;
 
+public class Book {
+	private String book;
+	private ArrayList<Paragraph> paragraphs;
+	
+	public Book(String book) {
+		this.book = book;
+	}
+	
+	public void add(Paragraph paragraph) {
+		this.paragraphs.add(paragraph);
+	}
+	
+	public String toString() {
+		return book;
+	}
+	
+	public int punctuations() {
+		int count = 0;
+		for (Paragraph p : paragraphs) {
+			count += p.punctuations();
+		}
+		return count;
+	}
+	
+	public int length() {
+		return paragraphs.size();
+	}
+	
+	public int numSentences() {
+		int count = 0;
+		for (Paragraph p : paragraphs) {
+			count += p.length();
+		}
+		return count;
+	}
+	
+	public float punctuationDensity() {
+		return (float) this.punctuations() / this.numSentences();
+	}
 }
