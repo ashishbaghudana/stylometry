3e7088f64385b341907313efce3dab5e33510a32
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-07 21:39:50 -0700

    Create Sentence structure

--- src/main/java/author/stylometry/structure/Sentence.java
+++ src/main/java/author/stylometry/structure/Sentence.java
@@ -1,5 +1,39 @@
 package main.java.author.stylometry.structure;
 
-public class Sentence {
+import java.util.ArrayList;
 
+public class Sentence {
+	String sentence;
+	ArrayList<Word> words;
+	
+	public Sentence(String sentence) {
+		this.sentence = sentence;
+	}
+	
+	public void add(String word) {
+		Word w = new Word(word);
+		words.add(w);
+	}
+	
+	public int punctuations() {
+		int count = 0;
+		for (Word word : words) {
+			if (word.isPunctuation()) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public int length() {
+		return words.size();
+	}
+	
+	public String toString() {
+		String s = "";
+		for (Word w : words) {
+			s += w.toString();
+		}
+		return s;
+	}
 }
