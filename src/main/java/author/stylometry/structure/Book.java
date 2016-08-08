8dfab6b76965c056ff1bc882ad6456e07529f5c6
Kumar <harshkumar@paypalcorp.com>
2016-08-08 10:23:34 +0530

    Paragraph

--- src/main/java/author/stylometry/structure/Book.java
+++ src/main/java/author/stylometry/structure/Book.java
@@ -1,5 +1,6 @@
 package main.java.author.stylometry.structure;
 
 public class Book {
+	
 
 }
--- src/main/java/author/stylometry/structure/Paragraph.java
+++ src/main/java/author/stylometry/structure/Paragraph.java
@@ -1,5 +1,37 @@
 package main.java.author.stylometry.structure;
 
-public class Paragraph {
+import java.util.ArrayList;
 
+public class Paragraph {
+	String paragraph;
+	ArrayList<Sentence> sentences;
+	
+	public Paragraph(String paragraph) {
+		this.paragraph = paragraph;
+	}
+	
+	public void add(String sentence) {
+		Sentence w = new Sentence(sentence);
+		sentences.add(w);
+	}
+	
+	public int punctuations() {
+		int count = 0;
+		for (Sentence sentence : sentences) {
+			count+=sentence.punctuations();		
+		}
+		return count;
+	}
+	
+	public int length() {
+		return sentences.size();
+	}
+	
+	public String toString() {
+		String p = "";
+		for (Sentence s : sentences) {
+			p += s.toString();
+		}
+		return p;
+	}
 }
