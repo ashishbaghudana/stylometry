c915a0777f558c46220878c5c501ccd9bde63eae
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-08 15:15:05 +0530

    Added main method to distinguish two authors

--- src/main/java/author/stylometry/App.java
+++ src/main/java/author/stylometry/App.java
@@ -1,13 +1,47 @@
 package main.java.author.stylometry;
 
+import main.java.author.stylometry.preprocess.Tokenizer;
+import main.java.author.stylometry.reader.GutenbergReader;
+import main.java.author.stylometry.structure.Book;
+import main.java.author.stylometry.structure.Collection;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main (String[] args) {
+    	GutenbergReader reader = new GutenbergReader();
+    	Tokenizer tokenizer = new Tokenizer("en-sent.bin", "en-token.bin");
+    	
+        Collection shakespeare = new Collection("Shakespeare");
+        shakespeare.add(new Book(reader.parseInput("books/romeojuliet.txt")));
+        shakespeare.add(new Book(reader.parseInput("books/juliuscaesar.txt")));
+        shakespeare.add(new Book(reader.parseInput("books/hamlet.txt")));
+        shakespeare.add(new Book(reader.parseInput("books/macbeth.txt")));
+        
+        shakespeare.preprocess(tokenizer);
+        shakespeare.calculateMetrics();
+        
+        Collection somerset = new Collection("Somerset");
+        somerset.add(new Book(reader.parseInput("books/themagician.txt")));
+        somerset.add(new Book(reader.parseInput("books/ofhumanbondage.txt")));
+        somerset.add(new Book(reader.parseInput("books/moonandsixpence.txt")));
+        
+        somerset.preprocess(tokenizer);
+        somerset.calculateMetrics();
+        
+        Collection unknown = new Collection("unknown");
+        unknown.add(new Book(reader.parseInput("books/ofhumanbondage.txt")));
+        
+        unknown.preprocess(tokenizer);
+        unknown.calculateMetrics();
+        
+        if (unknown.similarity(shakespeare) < unknown.similarity(somerset)) {
+        	System.out.println(shakespeare.author);
+        } else {
+        	System.out.println(somerset.author);
+        }
     }
 }
