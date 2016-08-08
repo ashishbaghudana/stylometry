fd5f33531e7ce2105159b97b25d9d6247efcb1fd
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-07 23:20:43 -0700

    Add Tokenizer class

--- src/main/java/author/stylometry/preprocess/Tokenizer.java
+++ src/main/java/author/stylometry/preprocess/Tokenizer.java
@@ -1,9 +1,62 @@
 package main.java.author.stylometry.preprocess;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
 public class Tokenizer {
 	
-	public Tokenizer() {
-		
+	private SentenceModel sentenceModel;
+	private TokenizerModel tokenModel;
+	private SentenceDetectorME sentenceDetector;
+	private TokenizerME wordTokenizer;
+	
+	public Tokenizer(String sentenceFile, String tokenFile) {
+		this.createSentenceModel(sentenceFile);
+		this.createTokenModel(tokenFile);
+		this.sentenceDetector = new SentenceDetectorME(this.sentenceModel);
+		this.wordTokenizer = new TokenizerME(this.tokenModel);
+	}
+	
+	private void createTokenModel(String file) {
+		InputStream modelIn = null;
+		try {
+			modelIn = new FileInputStream(file);
+			this.tokenModel = new TokenizerModel(modelIn);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (modelIn != null) {
+				try {
+					modelIn.close();
+				} catch (IOException e) {
+					
+				}
+			}
+		}
+	}
+	
+	private void createSentenceModel(String file) {
+		InputStream modelIn = null;
+		try {
+			modelIn = new FileInputStream(file);
+			this.sentenceModel = new SentenceModel(modelIn);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (modelIn != null) {
+				try {
+					modelIn.close();
+			    } catch (IOException e) {
+			    	e.printStackTrace();
+			    }
+			}
+		}
 	}
 	
 	public String[] paragraphs(String text) {
@@ -11,10 +64,10 @@ public class Tokenizer {
 	}
 	
 	public String[] sentences(String text) {
-		return null;
+		return this.sentenceDetector.sentDetect(text);
 	}
 	
 	public String[] words(String text) {
-		return null;
+		return wordTokenizer.tokenize(text);
 	}
 }
